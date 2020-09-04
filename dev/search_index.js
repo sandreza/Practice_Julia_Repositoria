923d151cef6a9249f6d594c4748fc442b9c1e1c6
Documenter.jl <documenter@juliadocs.github.io>
2020-09-04 20:54:35 +0000

    build based on 3b4b235

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Practice*Julia*Repositoria.jl","page":"PracticeJuliaRepositoria.jl","title":"PracticeJuliaRepositoria.jl","text":"","category":"section"},{"location":"","page":"PracticeJuliaRepositoria.jl","title":"PracticeJuliaRepositoria.jl","text":"Documentation for PracticeJuliaRepositoria.jl","category":"page"}]
+}

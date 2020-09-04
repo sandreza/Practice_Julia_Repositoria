3b4b235287c6778261417896eea6de560df180a3
sandreza <andrenogueirasouza@gmail.com>
2020-09-04 16:53:10 -0400

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [Practice_Julia_Repositoria]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/sandreza/Practice_Julia_Repositoria"
+)
